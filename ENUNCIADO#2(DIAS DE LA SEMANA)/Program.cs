f126f0fd4935ca4c689a7a71e7f028cd9110082a
Cristian Alexander Castillo Castillo <ccastilloc10@miumg.edu.gt>
2025-02-12 22:40:15 -0600

    Add files via upload

---
+++ ENUNCIADO#2(DIAS DE LA SEMANA)/Program.cs
@@ -0,0 +1,28 @@
+﻿int numero;
+
+Console.WriteLine("Digite un numero por favor:");
+numero = int.Parse(Console.ReadLine());
+
+if (numero == 1){
+    Console.WriteLine("Dia lunes");
+}else if (numero == 2){
+    Console.WriteLine("Dia martes");
+}
+else if (numero == 3){
+    Console.WriteLine("Dia miercoles");
+}
+else if (numero == 4){
+    Console.WriteLine("Dia jueves");
+}
+else if (numero == 5){
+    Console.WriteLine("Dia viernes");
+}
+else if (numero == 6){
+    Console.WriteLine("Dia sabado");
+}
+else if (numero == 7){
+    Console.WriteLine("Dia domingo");
+}
+else{
+    Console.WriteLine("Numero no valido");
+}
---
+++ ENUNCIADO#3(VERIFICACION DE ACCESO)/Program.cs
@@ -0,0 +1,15 @@
+﻿string usuario="castillo12";
+string password="UMG";
+
+Console.WriteLine("Ingrese su usuario por favor");
+string usuarioIngresado=Console.ReadLine();
+Console.WriteLine("Ingrese su contraseña por favor");
+string passwordIngresado=Console.ReadLine();
+
+if(usuarioIngresado==usuario && passwordIngresado==password){
+    Console.WriteLine("Acceso concedido");
+} if (usuarioIngresado==usuario && passwordIngresado!=password){
+    Console.WriteLine("Contraseña incorrecta");
+} if (usuarioIngresado!=usuario){
+    Console.WriteLine("El usuario no esta registrado");
+}
