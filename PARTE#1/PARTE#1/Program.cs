f46a1dafb711106cc5fc6ff45f56e03c98d6b31b
Cristian Alexander Castillo Castillo <ccastilloc10@miumg.edu.gt>
2025-02-12 22:38:59 -0600

    Add files via upload

---
+++ PARTE#1/PARTE#1/Program.cs
@@ -0,0 +1,16 @@
+﻿int numero;
+double numeroDecimal;
+string texto;
+bool booleano;
+
+Console.WriteLine();
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Digite un decimal: ");
+numeroDecimal = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Digite un texto: ");
+texto = Console.ReadLine() ?? string.Empty;
+Console.WriteLine("Digite un booleano: ");
+booleano = Convert.ToBoolean(Console.ReadLine());
+
+
---
+++ PARTE#3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicitar el primer valor numérico
+        Console.Write("Ingrese el primer valor numérico: ");
+        int valor1 = int.Parse(Console.ReadLine());
+
+        // Solicitar el segundo valor numérico
+        Console.Write("Ingrese el segundo valor numérico: ");
+        int valor2 = int.Parse(Console.ReadLine());
+
+        // Comparaciones
+        if (valor1 > valor2)
+        {
+            Console.WriteLine($"{valor1} es mayor que {valor2}");
+        }
+        else if (valor1 < valor2)
+        {
+            Console.WriteLine($"{valor1} es menor que {valor2}");
+        }
+        else
+        {
+            Console.WriteLine($"{valor1} es igual a {valor2}");
+        }
+
+        // Uso de AND (&&)
+        if (valor1 > 0 && valor2 > 0)
+        {
+            Console.WriteLine("Ambos valores son positivos.");
+        }
+        else
+        {
+            Console.WriteLine("Al menos uno de los valores no es positivo.");
+        }
+
+        // Uso de OR (||)
+        if (valor1 < 0 || valor2 < 0)
+        {
+            Console.WriteLine("Al menos uno de los valores es negativo.");
+        }
+        else
+        {
+            Console.WriteLine("Ninguno de los valores es negativo.");
+        }
+    }
+}
