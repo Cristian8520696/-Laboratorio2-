c556a232ff92b51c257f417160299876032e907e
Cristian Alexander Castillo Castillo <ccastilloc10@miumg.edu.gt>
2025-02-13 18:51:52 -0600

    Add files via upload

---
+++ ENUNCIADO#1(EDAD)/Program.cs
@@ -0,0 +1,16 @@
+﻿int edad;
+
+Console.WriteLine("Ingresa tu edad: ");
+edad = int.Parse(Console.ReadLine());
+if (edad < 12) {
+    Console.WriteLine("Eres un niño");
+}
+else if (edad >= 12 && edad <= 17) {
+    Console.WriteLine("Eres un adolescente");
+}
+else if (edad >= 18 && edad <= 59) {
+    Console.WriteLine("Eres un adulto");
+}
+else {
+    Console.WriteLine("Eres un adulto mayor");
+}
